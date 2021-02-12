3b4032e9b5bda6394f333da09d786c6da11d90d0
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-02-12 20:40:35 +0100

    Created bin/rv.py

---
+++ bin/rv.py
@@ -0,0 +1,19 @@
+"""rv
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-2-12"
+
+class rv:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(rv.rv())
+	
+	def rv():
+		"""rv first funtion
+		"""
+		return "rv" #for the test (see test_rv.py)
+		
+if __name__ == "__main__":
+	rv()
